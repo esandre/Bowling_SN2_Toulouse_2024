e20a7bd464750ff88b7e7e25fb491780dc08a482
Enzo Sandre <enzo@enzosandre.fr>
2024-03-05 17:18:15 +0100

    Inititalisation

--- Bowling.Test/PartieTest.cs
+++ Bowling.Test/PartieTest.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace Bowling.Test
 {
     public class PartieTest
@@ -10,6 +12,21 @@ namespace Bowling.Test
 
             // ALORS son score est de zéro
             Assert.Equal(0, partie.Score);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(9)]
+        public void UnLancer(int quillesTombées)
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on effectue un lancer tombant un nombre de quilles de 1 à 9
+            partie = partie.CompterLancer(quillesTombées);
+
+            // ALORS son score est de un
+            Assert.Equal(quillesTombées, partie.Score);
         }
     }
 }
--- Bowling/Partie.cs
+++ Bowling/Partie.cs
@@ -3,5 +3,19 @@
     public class Partie
     {
         public int Score { get; }
+
+        public Partie()
+        {
+        }
+
+        private Partie(int score)
+        {
+            Score = score;
+        }
+
+        public Partie CompterLancer(int quillesTombées)
+        {
+            return new Partie(quillesTombées);
+        }
     }
 }
