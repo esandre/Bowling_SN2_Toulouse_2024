82ccc0a73f00184d98540979d2208a2ec3beecc8
Enzo Sandre <enzo@enzosandre.fr>
2024-03-06 10:13:43 +0100

    Refacto pour clarifier le Spare

--- Bowling/Partie.cs
+++ Bowling/Partie.cs
@@ -15,25 +15,46 @@
 
         private Partie(Partie étatPrécédent, char action)
         { 
-            var premierLancerValide = int.TryParse(étatPrécédent.Représentation, out var nombreQuillesPremierLancer);
-            nombreQuillesPremierLancer = premierLancerValide ? nombreQuillesPremierLancer : 0;
-
-            var nombreQuillesSecondLancer = action == Strike ? NombreQuillesParFrame : int.Parse(action.ToString());
-
-            if (premierLancerValide && nombreQuillesPremierLancer + nombreQuillesSecondLancer == NombreQuillesParFrame) Représentation = Spare.ToString();
-            else Représentation = étatPrécédent.Représentation + action;
+            Représentation = étatPrécédent.Représentation + action;
         }
 
         public Partie CompterLancer(int quillesTombées)
         {
             if (EstTerminée()) return this;
 
+            if (TypeDernierLancer == TypeLancer.Numérique) return new Partie(this, Spare);
             if (quillesTombées == NombreQuillesParFrame) return new Partie(this, Strike);
+
             return new Partie(this, quillesTombées.ToString()[0]);
         }
 
         private int NombreDeStrike => Représentation.Count(c => c == Strike);
 
         private bool EstTerminée() => NombreDeStrike == NombreMaxLancersParPartieFullStrike;
+
+        private TypeLancer TypeDernierLancer
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Représentation)) 
+                    return TypeLancer.Aucun;
+
+                var last = Représentation.Last();
+                return last switch
+                {
+                    Strike => TypeLancer.Strike,
+                    Spare => TypeLancer.Spare,
+                    _ => TypeLancer.Numérique
+                };
+            }
+        }
+
+        private enum TypeLancer
+        {
+            Aucun,
+            Numérique,
+            Spare,
+            Strike
+        }
     }
 }
